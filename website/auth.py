eb5b23dc40097195eb33f44a7403685e12cdec26
Ahmed Saleem <ahmed.saleem1111@gmail.com>
2022-08-31 17:07:16 -0700

    Modifications 8/31/2022 4:56 PM

--- website/auth.py
+++ website/auth.py
@@ -1,40 +1,66 @@
-from distutils.command.build_scripts import first_line_re, flash
-import email
-from unicodedata import category
-from flask import Blueprint, render_template, request
-# storing roots for our site..., 
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
-auth = Blueprint('auth', __name__) # set up Blueprint for flask app (auth Blueprint)...
 
-@auth.route('/login', methods=['GET', 'POST']) # type requests it can accept...
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form # get data (from form attribute)
-    print(data)
-    return render_template("login.html", boolean=True)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template("logout.html")
+    logout_user()
+    return redirect(url_for('auth.login'))
+
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    # get all variables from form... (right?)
-    if request.method == "POST":
+    if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        
-        if len(email)<4: 
-            flash('Email must be greater than 3 characters.', category='error') # know what categories mean?
-        elif len(firstName) < 2: 
-            flash('First name must be greater than 1 characters.', category='error') # know what categories mean?
 
-        elif password1 != password2: 
-            flash('Passwords don\'t match.', category='error') # know what categories mean?
-        elif len(password1)<7: 
-            flash('Password must be at least 7 characters.', category='error') # know what categories mean?
-        else: # flash method on screen...
-            flash('Account created!', category='error') # know what categories mean?
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
